8c71dcfafda318ea2065d29783d4858f90890a4f
xumoyuns <xumoyunwebcoder@gmail.com>
2023-04-03 12:51:17 +0500

    counter full version

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // increase counter
+  const increase = function () {
+    setCount((count) => count + 1);
+  };
+  // decrease counter
+  const decrease = function () {
+    if (count > 0) {
+      setCount((count) => count - 1);
+    }
+  };
+
+  // reset
+  const reset = function () {
+    setCount((count) => (count = 0));
+  };
 
   return (
     <div className="App">
-      
-        
+      <h1 className="mb-4">{count}</h1>
+
+      <button onClick={increase} className="btn btn-primary">
+        +
+      </button>
+      <button
+        onClick={decrease}
+        className="btn btn-success mx-2"
+        disabled={count <= 0 ? true : false}
+      >
+        -
+      </button>
+      <button
+        onClick={reset}
+        className="btn btn-warning"
+        disabled={count === 0 ? true : false}
+      >
+        Reset
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
